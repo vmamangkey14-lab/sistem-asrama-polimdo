5725e619143bdf74caa0a3e23e90d161c0639064
Victoria <victoriamamngkey14@gmail.com>
2026-05-29 11:03:18 +0800

    fix db connection pakai env

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,15 +1,10 @@
 const mysql = require("mysql2/promise");
 
 const db = mysql.createPool({
-
-  host: "localhost",
-
-  user: "root",
-
-  password: "",
-
-  database: "sistem_asrama",
-
+  host: process.env.MYSQL_HOST,
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
+  database: process.env.MYSQL_DATABASE,
 });
 
 module.exports = db;
